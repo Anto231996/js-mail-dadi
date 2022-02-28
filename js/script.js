5485d8590a753f8a56764b751bdbf40a10cc6fe3
Anto231996 <tony2396@hotmail.it>
2022-02-28 15:29:04 +0100

    corretta es.mail

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ else if (userNumberRandom < botNumberRandom)
 console.log('YouLose!');
 else 
 console.log('Draw!');
+
+// -- EMAIL -- //
+const agreeEmails = ["abc@gmail.com", "antoniopagano996@gmail.com",  "abc@gmail.com", "abc@gmail.com", "abc@gmail.com", "abc@gmail.com", "abc@gmail.com"]
+
+let userEmail = prompt("Inserisci email");
+
+let isRegistered = false;
+
+for ( let i = 0 ; i < agreeEmails.length ; i++ ){
+    if( agreeEmails[i] == userEmail ){
+       isRegistered = true;
+    }
+}
+
+if (isRegistered){ 
+    console.log("l'utente e' registrato");
+} else {
+    console.log("l'utente non e' registrato");
+}
